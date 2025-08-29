6e7d28e25be7ed849bb11d7fb803119e09d484ea
Murat Ozcan <murat@Murats-MacBook-Pro.local>
2025-08-29 10:55:29 -0500

    how bad is Java?

--- src/01_intro/WelcomeToJava.java
+++ src/01_intro/WelcomeToJava.java
@@ -1,10 +1,7 @@
-// You learned about class in the previous video
 class WelcomeToJava {
 
-	// You'll learn about the main method next
     public static void main(String[] args) {
     
-    	// You'll learn about println statements too!
     	System.out.println("Welcome to Introduction to Java!");
     }
 }
