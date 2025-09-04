daea47ac034eac2500183b3889917c55d8b39b95
Murat Ozcan <murat@Murats-MacBook-Pro.local>
2025-09-04 06:53:06 -0500

    feat: comparators

---
+++ src/06_operators/Comparisons.java
@@ -0,0 +1,12 @@
+public class Comparisons {
+  public static void main(String[] args) {
+    final boolean first = 5 < 7;
+    final boolean second = 9 >= 2;
+    System.out.println("5 is smaller than 7: " + first);
+    System.out.println("9 is greater or equal to 2: " + second);
+
+    final int a = 193;
+    final boolean third = 193 >= a;
+    System.out.println("193 is greater or equal to a: " + third);
+  }
+}
