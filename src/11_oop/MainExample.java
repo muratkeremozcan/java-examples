d636a639210d3c5f9d7c1c4bff5d96d332a23ce1
Murat Ozcan <murat@mac.lan>
2025-09-17 09:21:48 -0500

    feat: inheritance 1

--- src/11_oop/MainExample.java
+++ src/11_oop/MainExample.java
@@ -4,21 +4,25 @@ public class MainExample {
   // Java supports nested classes
   // The static means it doesn't have access to Main's instance members
   // only the outer class can access the private members and methods, no others
-  private static final class MyCar {
+  private static class MyCar {
     public final String color;
     public final String model;
     public final int year;
     private final int vehicleNumber;
 
-    public MyCar() {
-      this.model = "camry";
-      this.color = "red";
-      this.year = 2019;
-      this.vehicleNumber = 101_189;
+    public MyCar(final String color, final String model, final int year, final int vehicleNumber) {
+      this.color = color;
+      this.model = model;
+      this.year = year;
+      this.vehicleNumber = vehicleNumber;
     }
 
     public void turnEngineOn() {
       System.out.println("engine is on");
+    }
+
+    public int getVehicleNumber() {
+      return vehicleNumber;
     }
 
     public int calculateMpg(final int milesDriven, final int gallonsUsed) {
@@ -30,13 +34,19 @@ public class MainExample {
     }
   }
 
+  public static final class Toyota extends MyCar {
+    public Toyota(final String color, final String model, final int year, final int vehicleNumber) {
+      super(color, model, year, vehicleNumber);
+    }
+  }
+
   /**
    * Main method to demonstrate the usage of the MyCar class.
    *
    * @param args command line arguments (not used)
    */
   public static void main(String[] args) {
-    final MyCar myCar = new MyCar();
+    final MyCar myCar = new MyCar("red", "camry", 2019, 101_189);
     System.out.println(myCar.color);
     System.out.println(myCar.model);
     System.out.println(myCar.year);
@@ -44,6 +54,18 @@ public class MainExample {
     myCar.turnEngineOn();
     System.out.println(myCar.calculateMpg(180, 20));
     myCar.deployAirbags();
+
+    System.out.println("\nChild class\n");
+    final Toyota myToyota = new Toyota("black", "yaris", 2014, 201_489);
+    System.out.println(myToyota.color);
+    System.out.println(myToyota.model);
+    System.out.println(myToyota.year);
+    // System.out.println(myToyota.vehicleNumber); // can't access private member
+    System.out.println(myToyota.getVehicleNumber());
+
+    myToyota.turnEngineOn();
+    System.out.println(myToyota.calculateMpg(180, 20));
+    // myToyota.deployAirbags(); // can't access private method
   }
 }
 
